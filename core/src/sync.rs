0465f692bd2a0f26ab2e82033d996b69aab3cd30
Yaroslav Petrov <yaroslav.v.petrov@yandex.ru>
2021-07-01 13:09:45 +0200

    Update sync.rs

--- core/src/sync.rs
+++ core/src/sync.rs
@@ -1,58 +1,101 @@
 use std::{
-    borrow::{Borrow, BorrowMut},
     ops::{Deref, DerefMut},
-    sync::{RwLock, RwLockReadGuard, RwLockWriteGuard},
+    sync::{
+        atomic::{AtomicUsize, Ordering::Relaxed},
+        Arc, RwLock, RwLockReadGuard, RwLockWriteGuard,
+    },
 };
 
-pub struct Lock<T>(RwLock<T>);
-impl<'a, T> Lock<T> {
-    pub fn lock(&'a self) -> ReadGuard<'a, T> {
-        ReadGuard(self.0.read().expect("Poisoning Error"))
-    }
+static GLOBAL_LOCK_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
-    pub fn lock_mut(&'a mut self) -> WriteGuard<'a, T> {
-        WriteGuard(self.0.write().expect("Poisoning Error"))
-    }
-}
-impl<T> From<T> for Lock<T> {
-    fn from(x: T) -> Self {
-        Lock(RwLock::new(x))
-    }
-}
+pub struct Lock<T>(usize, Arc<RwLock<T>>);
+
+pub struct ReadLock<T>(Lock<T>);
+
+pub struct Guard<'a, T>(RwLockWriteGuard<'a, T>);
 
 pub struct ReadGuard<'a, T>(RwLockReadGuard<'a, T>);
-impl<T> Borrow<T> for ReadGuard<'_, T> {
-    fn borrow(&self) -> &T {
-        self.0.borrow()
+
+impl<T> From<T> for Lock<T> {
+    fn from(x: T) -> Self {
+        Self(
+            GLOBAL_LOCK_COUNTER.fetch_add(1, Relaxed),
+            Arc::new(RwLock::new(x)),
+        )
     }
 }
+
+impl<T> Clone for Lock<T> {
+    fn clone(&self) -> Self {
+        let Self(id, lk) = self;
+        Self(id.clone(), lk.clone())
+    }
+}
+
+impl<T> Lock<T> {
+    pub fn id(&self) -> usize {
+        let Self(id, _) = self;
+        id.clone()
+    }
+
+    pub fn downgrade(&self) -> ReadLock<T> {
+        ReadLock(self.clone())
+    }
+
+    pub fn lock(&self) -> ReadGuard<'_, T> {
+        let Self(_, rw_lock) = self;
+        ReadGuard(rw_lock.read().unwrap())
+    }
+
+    pub fn lock_mut(&self) -> Guard<'_, T> {
+        let Self(_, rw_lock) = self;
+        Guard(rw_lock.write().unwrap())
+    }
+}
+
+impl<T> From<T> for ReadLock<T> {
+    fn from(x: T) -> Self {
+        ReadLock(x.into())
+    }
+}
+
+impl<T> Clone for ReadLock<T> {
+    fn clone(&self) -> Self {
+        let Self(inner) = self;
+        Self(inner.clone())
+    }
+}
+
+impl<T> ReadLock<T> {
+    pub fn id(&self) -> usize {
+        let Self(inner) = self;
+        inner.id()
+    }
+
+    pub fn lock(&self) -> ReadGuard<'_, T> {
+        let Self(inner) = self;
+        inner.lock()
+    }
+}
+
+impl<T> Deref for Guard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &*self.0
+    }
+}
+
+impl<T> DerefMut for Guard<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut *self.0
+    }
+}
+
 impl<T> Deref for ReadGuard<'_, T> {
     type Target = T;
-    fn deref(&self) -> &Self::Target {
-        self.0.borrow()
-    }
-}
-
-pub struct WriteGuard<'a, T>(RwLockWriteGuard<'a, T>);
-impl<T> Borrow<T> for WriteGuard<'_, T> {
-    fn borrow(&self) -> &T {
-        self.0.borrow()
-    }
-}
-impl<T> Deref for WriteGuard<'_, T> {
-    type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        self.0.borrow()
-    }
-}
-impl<T> BorrowMut<T> for WriteGuard<'_, T> {
-    fn borrow_mut(&mut self) -> &mut T {
-        self.0.borrow_mut()
-    }
-}
-impl<T> DerefMut for WriteGuard<'_, T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.0.borrow_mut()
+        &*self.0
     }
 }
