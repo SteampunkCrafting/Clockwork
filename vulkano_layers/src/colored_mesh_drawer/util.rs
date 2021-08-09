05f65d681ce9b8e249db8abbdf61ad19c520efcb
Yaroslav Petrov <yaroslav.v.petrov@yandex.ru>
2021-08-09 02:35:43 +0200

    Remove unused imports

--- vulkano_layers/src/colored_mesh_drawer/util.rs
+++ vulkano_layers/src/colored_mesh_drawer/util.rs
@@ -1,13 +1,5 @@
-use std::sync::Arc;
-
 use legion_ecs::prelude::Read;
 use physics::prelude::{nalgebra::Perspective3, RigidBodyHandle};
-use scene_utils::{mesh::Mesh, prelude::ColoredMesh};
-use vulkano::{
-    buffer::{BufferUsage, CpuAccessibleBuffer},
-    device::Device,
-    impl_vertex,
-};
 
 pub struct DrawMarker;
 pub struct Camera(pub Perspective3<f32>);
