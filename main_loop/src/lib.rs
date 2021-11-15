712d68087b63ca3f3f698867ba91c33d9b858db3
Yaroslav Petrov <yaroslav.v.petrov@yandex.ru>
2021-11-15 04:10:50 +0100

    Remove unused `Event` structure from main_loop crate

--- main_loop/src/lib.rs
+++ main_loop/src/lib.rs
@@ -1,9 +1,7 @@
-pub mod event;
 pub mod main_loop;
 pub mod state;
 
 pub mod prelude {
-    pub use crate::event::*;
     pub use crate::main_loop::*;
     pub use crate::state::*;
     pub use winit::event::VirtualKeyCode;
